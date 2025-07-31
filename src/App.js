11c7ff0dd5c539e38b45936b942ef76ce623fd31
Anne-sophie <anne.sophie.lesaint@gmail.com>
2025-07-31 19:19:36 +0200

    Feat (interface): Error handling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
+import Toast from "./components/Toast";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
   const [profile, setProfile] = useState([]);
   const [userName, setUserName] = useState('assigyn');
+  const [toast, setToast] = useState(null);
 
-  useEffect(() => {
-      axios(`https://api.github.com/users/${userName}`)
-          .then((response) => {
-             if (200 === response.status) {
-                 setProfile(response.data);
-             }
-          });
-  },[userName]);
+    useEffect(() => {
+        axios(`https://api.github.com/users/${userName}`)
+            .then((response) => {
+               if (200 === response.status) {
+                   setProfile(response.data);
+               }
+            })
+            .catch((response) => {
+                setToast({
+                    title: `Error ${response.status}`,
+                    text: response.message,
+                    type: 'error'
+                })
+            });
+    },[userName]);
 
-  return (
-    <div className="page-container">
+    useEffect(() => {
+        if (null !== toast) {
+            const toastMessage = document.getElementById('toast-message');
+            toastMessage.classList.remove('d-none');
+
+            setTimeout(() => {
+                toastMessage.classList.add('d-none');
+            }, 10000);
+        }
+    },[toast]);
+
+
+    return (
+    <div className="page-container position-relative">
       <Header setUserName={setUserName}/>
       <Nav profile={profile}/>
       <Profile profile={profile}/>
+      <Toast toast={toast}/>
     </div>
   );
 }
---
+++ src/components/Toast.js
@@ -0,0 +1,26 @@
+import styles from "./../assets/style/toast.module.css"
+
+function Toast({toast}) {
+    if (null === toast) {
+        return null;
+    }
+
+    function closeToast(link) {
+        document.getElementById('toast-message').classList.add('d-none');
+    }
+
+    return (
+        <div id="toast-message" className={`${styles.toast} d-flex-column d-none`}>
+            <div className="d-flex-row justify-content-between">
+                <p className="toast-title text-classic m-0">{toast.title}</p>
+                <a href="#" className="toast-close" onClick={(e) => {
+                    e.preventDefault();
+                    closeToast(e.target);
+                }}>x</a>
+            </div>
+            <p className="toast-description text-small mb-0">{toast.text}</p>
+        </div>
+    )
+}
+
+export default Toast
